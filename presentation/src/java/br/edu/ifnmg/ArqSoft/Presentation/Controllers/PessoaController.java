6d451bdf64abc429fd25aabed779d771c3bfe944
Petrônio Cândido <petronio.candido@gmail.com>
2019-11-19 10:41:04 -0300

    View pessoasEditar.xhtml e alterações nos controllers

--- presentation/src/java/br/edu/ifnmg/ArqSoft/Presentation/Controllers/PessoaController.java
+++ presentation/src/java/br/edu/ifnmg/ArqSoft/Presentation/Controllers/PessoaController.java
@@ -7,19 +7,19 @@ package br.edu.ifnmg.ArqSoft.Presentation.Controllers;
 
 import br.edu.ifnmg.ArqSoft.DomainModel.Pessoa;
 import br.edu.ifnmg.ArqSoft.DomainModel.PessoaRepositorio;
+import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
-import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
 
 /**
  *
  * @author petronio
  */
-@Named(value = "pessoaController")
+@ManagedBean(name = "pessoaController")
 @SessionScoped
-public class PessoaController {
+public class PessoaController implements Serializable  {
 
     
     private Pessoa pessoa;
@@ -45,30 +45,49 @@ public class PessoaController {
     }
 
     public PessoaRepositorio getRepositorio() {
-        return repositorio;
+        return this.repositorio;
     }
 
     public List<Pessoa> getListagem() {
-        return listagem;
+        if(listagem == null)
+            this.listagem = repositorio.Buscar(pessoa);
+        return this.listagem;
     }
 
     public void setListagem(List<Pessoa> listagem) {
         this.listagem = listagem;
     }
     
-    
-
     public void setRepositorio(PessoaRepositorio repositorio) {
         this.repositorio = repositorio;
     }
     
-    public String Novo(){
+    public String editar(int id){
+        this.pessoa = repositorio.Abrir(id);
+        return "pessoaEditar.xhtml";
+    }
+    
+    public String novo(){
         this.pessoa = new Pessoa();
         return "pessoaEditar.xhtml";
     }
     
     public String buscar(){
         this.listagem = repositorio.Buscar(this.getPessoa());
+        return "pessoaListar.xhtml";
+    }
+    
+    public void salvar(){
+        repositorio.Salvar(pessoa);
+    }
+    
+    public String cancelar() {
+        pessoa = new Pessoa();
+        return "pessoaListar.xhtml";
+    }
+    
+    public String apagar() {
+        repositorio.Apagar(pessoa);
         return "pessoaListar.xhtml";
     }
     
