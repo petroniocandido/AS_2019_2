0ea920e1cfd63ba7bd03555eb7ca90ff413d03ce
Petrônio Cândido <petronio.candido@gmail.com>
2019-11-19 10:48:48 -0300

    View pessoasEditar.xhtml e alterações nos controllers

--- presentation/src/java/br/edu/ifnmg/ArqSoft/Presentation/Controllers/PessoaController.java
+++ presentation/src/java/br/edu/ifnmg/ArqSoft/Presentation/Controllers/PessoaController.java
@@ -21,20 +21,24 @@ import javax.faces.bean.ManagedBean;
 @SessionScoped
 public class PessoaController implements Serializable  {
 
-    
+    // Objeto da Entidade sendo editada (Editar, Salvar, Apagar)
     private Pessoa pessoa;
     
+    // Listagem para buscas (Buscar, Cancelar)
     private List<Pessoa> listagem;
     
+    // Repositório de acesso ao BD
     @EJB
     private PessoaRepositorio repositorio;
     
-    /**
-     * Creates a new instance of PessoaController
-     */
+    // Construtor do Controller
     public PessoaController() {
        this.pessoa = new Pessoa();
     }
+    
+    /*
+    Getters e Setters
+    */
 
     public Pessoa getPessoa() {
         return pessoa;
@@ -62,30 +66,40 @@ public class PessoaController implements Serializable  {
         this.repositorio = repositorio;
     }
     
+    /*
+    Métodos que são usados nas views
+    */
+    
+    // Na listagem quando clica no link Editar
     public String editar(int id){
         this.pessoa = repositorio.Abrir(id);
         return "pessoaEditar.xhtml";
     }
     
+    // Na listagem quando clica no botão Novo
     public String novo(){
         this.pessoa = new Pessoa();
         return "pessoaEditar.xhtml";
     }
     
+    // Na listagem quando clica no botão Buscar
     public String buscar(){
         this.listagem = repositorio.Buscar(this.getPessoa());
         return "pessoaListar.xhtml";
     }
     
+    // Na edição quando clica no botão Salvar
     public void salvar(){
         repositorio.Salvar(pessoa);
     }
     
+    // Na edição quando clica no botão Cancelar
     public String cancelar() {
         pessoa = new Pessoa();
         return "pessoaListar.xhtml";
     }
     
+    // Na edição quando clica no botão Apagar
     public String apagar() {
         repositorio.Apagar(pessoa);
         return "pessoaListar.xhtml";
